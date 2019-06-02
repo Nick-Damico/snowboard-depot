2db8d875b016faeb0200509ff5699c38fb7df5f2
Nick-Damico <nickalan82@icloud.com>
2019-06-02 15:19:11 -0400

    add let factory_bot clean up tests

--- spec/controllers/line_items_controller_spec.rb
+++ spec/controllers/line_items_controller_spec.rb
@@ -1,24 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe LineItemsController, type: :controller do
+  let(:line_item) { create(:line_item) }
+  let(:product) { create(:product) }
+
   describe '#create' do
     it 'should create a line_item' do
       expect do
-        @product = create(:product)
-        post :create, params: { product_id: @product.id }
+        post :create, params: { product_id: product.id }
       end.to change(LineItem, :count).by(1)
     end
 
     it 'should return status 201' do
-      @product = create(:product)
-      post :create, params: { product_id: @product.id }
+      post :create, params: { product_id: product.id }
       expect(response).to have_http_status '201'
     end
   end
 
   describe '#update' do
     it 'should update a line_item' do
-      line_item = create(:line_item)
       patch :update, params: {id: line_item.id, line_item: { product_id: line_item.product_id } }
       expect(response).to redirect_to line_item_url(line_item)
     end
