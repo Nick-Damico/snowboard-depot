1dfeb17db52da25afb4d723e66a7e925baa475f7
Nick-Damico <nickalan82@icloud.com>
2019-06-01 22:42:09 -0400

    add test for #add_product

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -2,4 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
   it { should have_many(:line_items) }
+
+  describe '#add_product' do
+    it 'creates a line_item in cart' do
+      cart = create(:cart)
+      product = create(:product)
+      cart.add_product(product)
+      cart.save!
+      expect(cart.line_items.where(product_id: product.id).count).to eq(1)
+    end
+  end
 end
