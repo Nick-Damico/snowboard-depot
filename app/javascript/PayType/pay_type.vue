213533f6c9d3b991557a9618293c4d053a72c8eb
Nick-Damico <nickalan82@icloud.com>
2019-08-29 22:43:07 -0400

    Add PO fields, Dynamic payment form complete.

--- app/javascript/PayType/pay_type.vue
+++ app/javascript/PayType/pay_type.vue
@@ -1,24 +1,48 @@
 <template lang="html">
   <div id="app">
-    <select class="pay-type" name="order[pay_type_id]">
+    <select @change="selectType($event.target.value)" class="pay-type" name="order[pay_type_id]">
       <option value="">Select A Pay Type</option>
       <option value="1">Check</option>
       <option value="2">Credit Card</option>
       <option value="3">Purchase Order</option>
     </select>
-    <CreditCardFields />
+
+    <div v-if="selectedType === 1">
+      <CheckFields />
+    </div>
+    <div v-else-if="selectedType === 2">
+      <CreditCardFields />
+    </div>
+    <div v-else-if="selectedType === 3">
+      <PurchaseOrderFields />
+    </div>
+    <div v-else>
+    </div>
+
   </div>
 </template>
 
 <script>
 import CheckFields from './check_fields.vue'
 import CreditCardFields from './credit_card_fields.vue'
+import PurchaseOrderFields from './purchase_order_fields.vue'
 
 export default {
   name: "PayType",
+  data() {
+    return {
+      selectedType: null
+    }
+  },
   components: {
     CheckFields,
-    CreditCardFields
+    CreditCardFields,
+    PurchaseOrderFields
+  },
+  methods: {
+    selectType(id) {
+      this.selectedType = parseInt(id)
+    }
   }
 }
 </script>
---
+++ app/javascript/PayType/purchase_order_fields.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <div class="field">
+    <label for="order_po_number">PO #</label>
+    <input
+      id="order_po_number"
+      type="password"
+      name="order[po_number]"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PurchaseOrderFields'
+}
+</script>
+
+<style lang="css" scoped>
+</style>
