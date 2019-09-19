ba508b646b867dc4b51bfc23c960be5848bbc8bf
Nick-Damico <nickalan82@icloud.com>
2019-09-18 22:55:49 -0400

    Configure action_mailer delivery_method to use smtp.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,24 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :letter_opener
+  # Uncomment to use RailsCast developed gem letter_opener
+  # config.action_mailer.delivery_method = :letter_opener
+
+  ## Gmail configuration
+  config.action_mailer.delivery_method = :smtp
+  host = 'localhost:3000' #replace with your own url
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+      :address              => 'smtp.gmail.com',
+      :port                 => 587,
+      :user_name            => ENV["GMAIL_USERNAME"],
+      :password             => ENV["GMAIL_PASSWORD"],
+      :authentication       => "plain",
+      :enable_starttls_auto => true
+  }
+
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.perform_caching = false
