9b51d0b8303d17eaae7dc803241cbf502fdab04d
Nick-Damico <nickalan82@icloud.com>
2019-09-19 22:16:17 -0400

    Customize shipped email html/text format msgs. Add shipped to previewer

--- lib/mailer_previews/order_mailer_preview.rb
+++ lib/mailer_previews/order_mailer_preview.rb
@@ -3,9 +3,9 @@ class OrderMailerPreview < ActionMailer::Preview
     order = Order.includes(:line_items).where.not(line_items: { id: nil }).last
     OrderMailer.received(order)
   end
-  # def shipped(order)
-  #   @order = order
-  #
-  #   mail to: order.email, subject: 'Pragmatic Store Order Shipped'
-  # end
+
+  def shipped
+    order = Order.includes(:line_items).where.not(line_items: { id: nil }).last
+    OrderMailer.shipped(order)
+  end
 end
