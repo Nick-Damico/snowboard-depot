79d2ad5682d5a9e2f87b49a3c78ae3cabdaf2c4f
Nick-Damico <nickalan82@icloud.com>
2019-09-28 11:22:34 -0400

    Feature spec for checkout, via payment type Check. Test Mailer, ActiveJob

--- spec/features/orders_spec.rb
+++ spec/features/orders_spec.rb
@@ -3,6 +3,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Orders', type: :feature do
+  include ActiveJob::TestHelper
+
+  let(:user) {
+    {
+      name: 'Johnny Appleseed',
+      address: '123 Apple Orchard Rd',
+      email: 'johhny_apple@orchard.com'
+    }
+  }
+
+  after do
+    Order.delete_all
+    LineItem.delete_all
+  end
+
   scenario 'user visits store page' do
     visit root_path
     expect(page).to have_content('The Pragmatic Bookshelf')
@@ -24,9 +39,9 @@ RSpec.describe 'Orders', type: :feature do
     expect(page).to have_current_path(new_order_path)
     expect(page).to have_content('Please Enter Your Details')
 
-    fill_in 'order_name', with: 'Johnny Appleseed'
-    fill_in 'order_address', with: '123 Main Street'
-    fill_in 'order_email', with: 'johnny_apple@trees.com'
+    fill_in 'order_name', with: user[:name]
+    fill_in 'order_address', with: user[:address]
+    fill_in 'order_email', with: user[:email]
     assert_no_selector '#order_routing_number'
     select('Check', from: 'pay_type')
     assert_selector '#order_routing_number'
@@ -41,9 +56,9 @@ RSpec.describe 'Orders', type: :feature do
     expect(page).to have_current_path(new_order_path)
     expect(page).to have_content('Please Enter Your Details')
 
-    fill_in 'order_name', with: 'Johnny Appleseed'
-    fill_in 'order_address', with: '123 Main Street'
-    fill_in 'order_email', with: 'johnny_apple@trees.com'
+    fill_in 'order_name', with: user[:name]
+    fill_in 'order_address', with: user[:address]
+    fill_in 'order_email', with: user[:email]
     assert_no_selector '#order_credit_card_number'
     assert_no_selector '#order_expiration_date'
     select('Credit Card', from: 'pay_type')
@@ -60,11 +75,45 @@ RSpec.describe 'Orders', type: :feature do
     expect(page).to have_current_path(new_order_path)
     expect(page).to have_content('Please Enter Your Details')
 
-    fill_in 'order_name', with: 'Johnny Appleseed'
-    fill_in 'order_address', with: '123 Main Street'
-    fill_in 'order_email', with: 'johnny_apple@trees.com'
+    fill_in 'order_name', with: user[:name]
+    fill_in 'order_address', with: user[:address]
+    fill_in 'order_email', with: user[:email]
     assert_no_selector '#order_po_number'
     select('Purchase Order', from: 'pay_type')
     assert_selector '#order_po_number'
+  end
+
+  scenario 'check routing number', js: true do
+    visit root_path
+
+    # Place Order
+    first('.catalog li').click_on('Add to Cart')
+    click_button('Checkout')
+    fill_in 'order_name', with: user[:name]
+    fill_in 'order_address', with: user[:address]
+    fill_in 'order_email', with: user[:email]
+    select('Check', from: 'pay_type')
+    fill_in 'Routing #', with: '054321'
+    fill_in 'Acct #', with: '123456789'
+
+    # Expect Order, LineItem to update.
+    perform_enqueued_jobs do
+      click_button 'Place Order'
+    end
+
+    expect(Order.count).to eq(1)
+    expect(LineItem.count).to eq(1)
+    # Check Order info is correct
+    order = Order.first
+    expect(order.name).to eq(user[:name])
+    expect(order.address).to eq(user[:address])
+    expect(order.email).to eq(user[:email])
+    expect(order.pay_type.name).to eq('Check')
+
+    # Check Email was sent.
+    mail = ActionMailer::Base.deliveries.last
+    expect(mail.to.first).to eq(user[:email])
+    expect(mail.from).to eq('RailsDepotAdmin admin@rails-depot.com')
+    expect(mail.subject).to eq('Pragmatic Store Order Confirmation')
   end
 end
