01ed845b26190cea2536062e39d61076a4f67dfc
Nick-Damico <nickalan82@icloud.com>
2021-02-28 15:14:52 -0500

    Feat: Improve display of Products and forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+    def btn_text(record)
+        class_name = record.class.name
+        record.persisted? ? "Update #{class_name}" : "Create #{class_name}"
+    end
 end
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,4 +2,9 @@ module ProductsHelper
     def display_price(price=0)
         number_to_currency(price, precision: 2) 
     end
+
+    def display_description(description, truncate=false, length=100)
+        desc = strip_tags(description)
+        truncate ? truncate(desc, length: length) : desc
+    end
 end
